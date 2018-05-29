4f6bb8bd172c5b9f4b5e8abd45879159944304f0
Mohamed Romdhani <mromdhani7@gmail.com>
2018-05-29 01:15:36 +0100

    Upgrade to Angular 6 + Bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsFilterPipe } from './pipes/products-filter.pipe';
+import { StarComponent } from './components/shared/star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductsFilterPipe,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
